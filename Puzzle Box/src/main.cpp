f090c26161006f7090cd2684d0d9e789154bbbf2
Will <will_luton@hotmail.com>
2017-10-21 14:03:03 +0100

    Initial commit

---
+++ Puzzle Box/src/main.cpp
@@ -0,0 +1,116 @@
+/*
+  Escape Box
+ */
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+
+//Card reader
+constexpr uint8_t RST_PIN = 9;     // Configurable, see typical pin layout above
+constexpr uint8_t SS_PIN = 10;     // Configurable, see typical pin layout above
+
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+
+MFRC522::MIFARE_Key key;
+
+//Knocking Defines
+#define Solenoid 7
+#define SolenoidOnDelay 20
+#define KnockInDigitDelay 750
+#define KnockBetweenDigitDelay 1500
+
+bool DoKnocking = true;
+
+byte Combination[] = { 3, 2, 1, 4}; //Lock combination
+byte Mode = 0; //Board mode
+
+///////////////////////////////////////////////////////////
+void setup() {
+
+  Serial.begin(9600); //Init serial
+  SPI.begin(); // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522 (card reader)
+
+  Serial.println("Serial started...");
+
+  pinMode(Solenoid, OUTPUT); // initialize the solenoid
+
+}
+
+///////////////////////////////////////////////////////////
+void Knock(){
+
+  byte Digit = 0;
+
+  byte KnockInDigit = 0;
+
+  while (Digit < sizeof(Combination)){
+
+    while (KnockInDigit < Combination[Digit]){
+
+      delay(KnockInDigitDelay);
+
+      digitalWrite(Solenoid, HIGH);
+
+      delay(SolenoidOnDelay);
+
+      digitalWrite(Solenoid, LOW);
+
+      KnockInDigit++;
+
+    }
+
+    KnockInDigit = 0;
+
+    delay(KnockBetweenDigitDelay);
+
+    Digit++;
+
+  }
+
+  Mode = 0;
+
+}
+
+///////////////////////////////////////////////////////////
+void loop() {
+
+  switch (Mode){
+
+    case 0:
+
+      //Delay
+
+      delay(500);
+
+      // Look for  cards
+      if (rfid.PICC_IsNewCardPresent()) {
+
+        Serial.println("Card found.");
+
+        Mode = 1;
+
+      } else if (!rfid.PICC_IsNewCardPresent()) {
+
+          Serial.println("No card.");
+
+          DoKnocking = true;
+
+      }
+
+      break;
+
+    case 1: //Do the knock
+
+      Serial.println("Called knock.");
+
+      if (DoKnocking) Knock();
+
+      DoKnocking = false;
+
+      break;
+
+  }
+}
